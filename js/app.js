5e05bab74f5bd6363798a505c13b01932e4da316
Joy Dey <joydey.bd.2002@gmail.com>
2021-07-21 13:57:47 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Navigation
+function toggleMenu() {
+  let navigation = document.querySelector(".navigation");
+  let toggle = document.querySelector(".toggle");
+  navigation.classList.toggle("active");
+  toggle.classList.toggle("active");
+}
